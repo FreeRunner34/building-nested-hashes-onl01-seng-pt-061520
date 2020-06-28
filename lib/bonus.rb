241278b95869f61afc0e745366c490ed9ea8a4b2
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-28 02:09:44 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,7 +24,7 @@ def bonus
   
 #epic_tragedy(hero(status(dead)))
 #epic_tragedy(Capulet(heroine(status)))
-
+epic_tragedy[:hero[:status]] = "Dead"
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
